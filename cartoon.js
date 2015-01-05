e8d6b4a23220229280eb5176ee652a0a1105c6f9
Asgeir Hølleland <asgeir.holleland@gmail.com>
2015-01-05 22:19:31 +0100

    Added process arguments for fetching a specific cartoon from today, also support guest series

--- cartoon.js
+++ cartoon.js
@@ -3,10 +3,26 @@ var url = require('url');
 var fs = require('fs');
 
 var SCRIPTURL = 'http://www.dagbladet.no/tegneserie/pondusarkiv/serveconfig.php?';
-var STRIP = 'lunch';
+var STRIPROOT = './strips/';
+var GUESTFOLDER = STRIPROOT+'gjesteserie/';
+
+var STRIP = process.argv[2];
+var STRIPNAME = STRIP;
+var isGuestSeries = false;
+if (STRIP == null) {
+  console.log('ERROR: Pass in the name of the strip');
+  process.exit(0);
+} else {
+  if (STRIP.indexOf('gjesteserie') >= 0) {
+    isGuestSeries = true;
+    STRIPNAME = STRIP.substring(STRIP.indexOf('/')+1);
+  }
+}
+
 var date = new Date();
 var reqUrl = SCRIPTURL + 'date=' + getTime(date) + '&strip=' + STRIP;
 var filename = getFilename(date);
+
 
 getCartoonLocation(reqUrl);
 
@@ -23,24 +39,13 @@ function getCartoonData(rawUrl) {
   var urlObj = url.parse(rawUrl);
   console.log(urlObj);
   http.get(urlObj, function(res) {
-    fs.exists('./strips/' + STRIP, function(exists) {
+    fs.exists(STRIPROOT, function(exists) {
       if (exists) {
-        writeData(res);
+        checkIsGuest(res);
       } else {
-        fs.exists('./strips', function(exists) {
-          if (exists) {
-            fs.mkdir('./strips/' + STRIP, function() {
-              writeData(res);
-            });
-          } else {
-            fs.mkdir('./strips', function() {
-              fs.mkdir('./strips/' + STRIP, function() {
-                writeData(res);
-              });
-            });
-          }
-        })
-
+        fs.mkdir(STRIPROOT, function() {
+          checkIsGuest(res);
+        });
       }
     });
   }).on('error', function(e) {
@@ -48,8 +53,52 @@ function getCartoonData(rawUrl) {
   });
 }
 
-function writeData(res) {
-  var file = fs.createWriteStream('./strips/' + STRIP + '/' + filename);
+function checkIsGuest(res) {
+  if (!isGuestSeries) {
+    checkStripFolder(res);
+  } else {
+    checkStripFolder(res, true);
+  }
+}
+
+function checkStripFolder(res, isGuest, guestCreated) {
+  if (!isGuest) {
+    fs.exists(STRIPROOT+STRIP, function(exists) {
+      if (exists) {
+        writeData(res, STRIPROOT+STRIP);
+      } else {
+        fs.mkdir(STRIPROOT+STRIP, function() {
+          writeData(res, STRIPROOT+STRIP);
+        });
+      }
+    });
+  } else {
+    if (!guestCreated) {
+      fs.exists(GUESTFOLDER, function(exists) {
+        if (exists) {
+          checkStripFolder(res, true, true);
+        } else {
+          fs.mkdir(GUESTFOLDER, function() {
+            checkStripFolder(res, true, true);
+          });
+        }
+      });
+    } else {
+      fs.exists(GUESTFOLDER+STRIPNAME, function(exists) {
+        if (exists) {
+          writeData(res, GUESTFOLDER+STRIPNAME);
+        } else {
+          fs.mkdir(GUESTFOLDER+STRIPNAME, function() {
+            writeData(res, GUESTFOLDER+STRIPNAME);
+          });
+        }
+      });
+    }
+  }
+}
+
+function writeData(res, filepath) {
+  var file = fs.createWriteStream(filepath + '/' + filename);
   res.on('data', function(data) {
     file.write(data);
   }).on('end', function() {
